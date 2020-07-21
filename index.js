2105f76860ca53ff308ed8589cecb1d529732c3a
Yevhen Zalevskyy <360a3.eugenio@gmail.com>
2020-07-21 07:58:15 +0200

    without space

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ let sum = 0;
 
 do {
   N = +prompt('Enter integer N');
-  if (Number.isInteger(N)) {
+  if (Number.isInteger(N) && N != '') {
     continue;
   }
   alert ('It is not integer. Try again');
-} while (!Number.isInteger(N));
+} while (!Number.isInteger(N) || N == '');
 
 do {
   M = +prompt('Enter integer M');
-  if (Number.isInteger(M)) {
+  if (Number.isInteger(M) && M != '') {1
     if (M <= N) {
       alert (`The number must be greater than ${N}. Try again`)
     }
     continue;
   }
   alert ('It is not integer. Try again');
-} while (!Number.isInteger(M) || M <= N);
+} while (!Number.isInteger(M) || M == '' || M <= N);
 
 isEven = confirm('Skip even numbers?')
 
